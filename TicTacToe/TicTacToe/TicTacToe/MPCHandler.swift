3f2fda01e5ec8323bb69ae7fd7dd645666b7557f
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-09-19 12:17:47 +0100

    Created a multi-player TicTacToe game following a tutorial from Brian Advent. Using the MultiPeer Connectivity framework. There is still some work that can be done to this.

---
+++ TicTacToe/TicTacToe/TicTacToe/MPCHandler.swift
@@ -0,0 +1,13 @@
+//
+//  MPCHandler.swift
+//  TicTacToe
+//
+//  Created by DavidMcQueen on 19/09/2014.
+//  Copyright (c) 2014 DavidMcQueen. All rights reserved.
+//
+
+import UIKit
+
+class MPCHandler: NSObject {
+   
+}
---
+++ TicTacToe/TicTacToe/TicTacToe/TTTImageView.swift
@@ -0,0 +1,22 @@
+//
+//  TTTImageView.swift
+//  TicTacToe
+//
+//  Created by DavidMcQueen on 19/09/2014.
+//  Copyright (c) 2014 DavidMcQueen. All rights reserved.
+//
+
+import UIKit
+
+class TTTImageView: UIImageView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
