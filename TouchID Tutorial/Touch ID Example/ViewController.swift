b056b8097e008ea29878c1d6f8007498c54b9d5f
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-09-29 20:20:34 +0100

    Init of TouchID tutorial

---
+++ TouchID Tutorial/Touch ID Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Touch ID Example
+//
+//  Created by Adam Leonard on 9/27/14.
+//  Copyright (c) 2014 com.swiftcast.tv. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+
+    @IBOutlet var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
