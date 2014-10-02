b040d61e524aed9b9b4a33a5017e68313701d944
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-10-02 19:53:37 +0100

    Created a test story board, to see which each of the options (SPA, Tabbed etc) has to offer and which would be best for 3rd year project

---
+++ Storyboards/TestStoryboard/TestStoryboard/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TestStoryboard
+//
+//  Created by DavidMcQueen on 02/10/2014.
+//  Copyright (c) 2014 DavidMcQueen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
