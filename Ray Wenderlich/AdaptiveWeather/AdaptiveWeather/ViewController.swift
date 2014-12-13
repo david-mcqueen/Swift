3b5c6bf5d8cc0a762469a916b03d3ff1123c411c
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-12-13 14:49:55 +0000

    Tutorial on Adaptive Layout

---
+++ Ray Wenderlich/AdaptiveWeather/AdaptiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by DavidMcQueen on 13/12/2014.
+//  Copyright (c) 2014 David McQueen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
